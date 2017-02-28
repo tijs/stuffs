aadea8375edc7cdc460c15834a211d011a507953
Tijs Teulings <hello@tijs.org>
2017-02-28 16:34:54 +0100

    added logging. and that hot redux stuff looks scary. i've never used it. might work though. i just hit command-r to reload like a caveman

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,21 +1,34 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import reducers from '../reducers';
+import createLogger from 'redux-logger';
+//import reducers from '../reducers';
+
+import rootReducer from '../reducers';
+
 
 export default function configureStore() {
+  let middleware = [thunk];
+
+  if (__DEV__) {
+    const logger = createLogger({
+      level: 'info',
+      collapsed: false,
+    });
+    middleware = [...middleware, logger];
+  }
+
   const store = createStore(
-    reducers,
-    applyMiddleware(thunk)
+    rootReducer,
+    undefined,
+    applyMiddleware(...middleware),
   );
 
-  if (module.hot) {
-    // Enable Webpack hot module replacement for reducers
-    module.hot.accept('../reducers', () => {
-      const nextRootReducer = require('../reducers');
-
-      store.replaceReducer(nextRootReducer);
-    });
-  }
+  // if (module.hot) {
+  //   // Enable Webpack hot module replacement for reducers
+  //   module.hot.accept('../reducers', () => {
+  //     store.replaceReducer(rootReducer);
+  //   });
+  // }
 
   return store;
 }
